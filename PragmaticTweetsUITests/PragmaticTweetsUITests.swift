b6a6a81c85800cfece57b5aff4d4a15702230b81
Aldus Botha <aldus@aldus-bothas-macbook-pro.local>
2015-07-17 09:38:54 +0200

    -Added UI tests.

--- PragmaticTweetsUITests/PragmaticTweetsUITests.swift
+++ PragmaticTweetsUITests/PragmaticTweetsUITests.swift
@@ -1,35 +1,70 @@
 //
-//  PragmaticTweetsUITests.swift
-//  PragmaticTweetsUITests
+//  PragmaticTweets_UI_Tests.swift
+//  PragmaticTweets UI Tests
 //
-//  Created by Aldus Botha on 2015/07/01.
+//  Created by Aldus Botha on 2015/07/16.
 //  Copyright © 2015 Aldus Botha. All rights reserved.
 //
 
 import Foundation
 import XCTest
 
-class PragmaticTweetsUITests: XCTestCase {
-        
+class PragmaticTweets_UI_Tests: XCTestCase {
+    
+    var dateFormatter = NSDateFormatter()
+    
     override func setUp() {
         super.setUp()
-        
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+        dateFormatter.dateFormat = "YYYY/MM/DD hh:mm"
         continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         XCUIApplication().launch()
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testNavigateToUserDetailView() {
+        let app = XCUIApplication()
+        sleep(3)
+        
+        let firstCell = app.tables.cells.elementAtIndex(0)
+        let cellTweetUserName = firstCell.staticTexts["cellUserName"].value as! String
+        print("Value of cellUserName")
+        print(firstCell.staticTexts["cellUserName"].value!)
+        firstCell.tap()
+        sleep(3)
+        
+        let tweetDetailUserName = (app.staticTexts["tweetDetailUserName"]).value  as! String
+        XCTAssertEqual(cellTweetUserName, tweetDetailUserName)
+        (app.buttons["tweetDetailUserButton"]).tap()
+        sleep(3)
+        
+        let userDetailUserName = (app.staticTexts["userDetailUserName"]).value as! String
+        XCTAssertEqual(cellTweetUserName, userDetailUserName)
+        
+        (app.images["userDetailImageView"]).tap()
+        sleep(3)
+        
+        app.buttons["Done"].tap()
+        
+        app.buttons["Done"].tap()
+        
+        app.buttons["Tweets"].tap()
+    }
+    
+    func testSendTweet() {
+        let app = XCUIApplication()
+        app.navigationBars["Tweets"].buttons["Add"].tap()
+        app.tables.textViews.elementAtIndex(0).typeText("Test tweet - " + dateFormatter.stringFromDate(NSDate()))
+        app.navigationBars["Twitter"].buttons["Post"].tap()
+    }
+    
+    func testSendImageTweet() {
+        let app = XCUIApplication()
+        app.navigationBars["Tweets"].buttons["Camera"].tap()
+        app.tables.textViews.elementAtIndex(0).typeText("Test image tweet - " + dateFormatter.stringFromDate(NSDate()))
+        app.navigationBars["Twitter"].buttons["Post"].tap()
     }
     
 }
